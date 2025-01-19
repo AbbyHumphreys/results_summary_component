d5bcec3c3fda75a96c9d295e9ae383b9310b99ce
bibby3316 <bibby3316@gmail.com>
2025-01-19 08:10:28 +0000

    style: breakpoints

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,8 @@ export default function Card () {
             <div className="card">
                 <h2 className="card-header">Your Result</h2>
                 <Circle />
+                <h1>Great</h1>
+                <p className="score-paragraph">You scored higher than 65% of the people who have taken these test.</p>
             </div>
         </>
     )

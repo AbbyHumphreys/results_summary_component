901b708c344cc2f980fa0016dcdc90e1e7bb186a
bibby3316 <bibby3316@gmail.com>
2025-11-23 15:58:36 +0000

    fix container layout and border radius

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,14 @@
-export default function Rating ({ratingType, score, icon}) {
-    const ratingClass = `rating-container ${ratingType.toLowerCase()}`
-    return (
-        <>
-            <div className={ratingClass}>
-                <img className="rating-icon" src={icon} alt="icon"/>
-                <p className="rating-text">{ratingType}</p>
-                <p className="rating">{score}/ 100</p>
-            </div>
-        </>
-    )
+export default function Rating({ ratingType, score, icon }) {
+  const ratingClass = `rating-container ${ratingType.toLowerCase()}`;
+
+  const iconUrl = `${process.env.PUBLIC_URL}${icon}`; 
+  // PUBLIC_URL will be "/results_summary_component" in production
+
+  return (
+    <div className={ratingClass}>
+      <img className="rating-icon" src={iconUrl} alt={`${ratingType} icon`} />
+      <p className="rating-text">{ratingType}</p>
+      <p className="rating">{score} / 100</p>
+    </div>
+  );
 }
